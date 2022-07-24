7f499083f5a29b5b906fb066292d8420f268e0d7
James Dowell <jamesdowell@Jamess-MacBook-Pro.local>
2022-07-24 11:30:12 -0700

    add resolver, fix prettierrc

--- server/src/apollo-server.ts
+++ server/src/apollo-server.ts
@@ -3,37 +3,60 @@ import { ApolloServer, ExpressContext, gql } from 'apollo-server-express';
 import * as express from 'express';
 import { Server } from 'http';
 import Db from './db';
+
 export async function createApolloServer(
-  _db: Db,
-  httpServer: Server,
-  app: express.Application
+    db: Db,
+    httpServer: Server,
+    app: express.Application
 ): Promise<ApolloServer<ExpressContext>> {
-  const typeDefs = gql`
-    type Query {
-      currentUser: User!
-      suggestions: [Suggestion!]!
-    }
-    type User {
-      id: String!
-      name: String!
-      handle: String!
-      coverUrl: String!
-      avatarUrl: String!
-      createdAt: String!
-      updatedAt: String!
-    }
-    type Suggestion {
-      name: String!
-      handle: String!
-      avatarUrl: String!
-      reason: String!
-    }
-  `;
-  const server = new ApolloServer({
-    typeDefs,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
-  await server.start();
-  server.applyMiddleware({ app });
-  return server;
+    const typeDefs = gql`
+        type Query {
+            currentUser: User!
+            suggestions: [Suggestion!]!
+        }
+        type User {
+            id: String!
+            name: String!
+            handle: String!
+            coverUrl: String!
+            avatarUrl: String!
+            createdAt: String!
+            updatedAt: String!
+        }
+        type Suggestion {
+            name: String!
+            handle: String!
+            avatarUrl: String!
+            reason: String!
+        }
+    `;
+
+    const resolvers = {
+        Query: {
+            currentUser: () => {
+                return {
+                    id: '123',
+                    name: 'John Doe',
+                    handle: 'johndoe',
+                    coverUrl: '',
+                    avatarUrl: '',
+                    createdAt: '',
+                    updatedAt: '',
+                };
+            },
+            suggestions: () => {
+                return [];
+            },
+        },
+    };
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: () => ({ db }),
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+    await server.start();
+    server.applyMiddleware({ app });
+    return server;
 }
